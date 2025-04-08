54fb00e52920213c4f366f98a9a808a5f698a151
Airat Nazmiev <43406093+AiratNazmiev@users.noreply.github.com>
2025-04-08 10:38:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -66,15 +66,21 @@ text_value = st.text_area(
 ### load translators:
 @st.cache_resource
 def load_translators():
-    ru_en_tokenizer = transformers.AutoTokenizer.from_pretrained("AiratNazmiev/Helsinki-NLP-opus-mt-ru-en-tokenizer", local_files_only=False)
-    ru_en_model = transformers.AutoModelForSeq2SeqLM.from_pretrained("AiratNazmiev/Helsinki-NLP-opus-mt-ru-en-model", local_files_only=False)
-    #ru_en_tokenizer.save_pretrained("./local_models/opus-mt-ru-en")
-    #ru_en_model.save_pretrained("./local_models/opus-mt-ru-en")
+    ru_en_tokenizer = transformers.AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ru-en", use_auth_token=hf_token)
+    ru_en_model = transformers.AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-ru-en", use_auth_token=hf_token)
+    ru_en_tokenizer.save_pretrained("./local_models/opus-mt-ru-en")
+    ru_en_model.save_pretrained("./local_models/opus-mt-ru-en")
+    
+    ru_en_tokenizer = transformers.AutoTokenizer.from_pretrained("./local_models/opus-mt-ru-en", local_files_only=True)
+    ru_en_model = transformers.AutoModelForSeq2SeqLM.from_pretrained("./local_models/opus-mt-ru-en", local_files_only=True)
 
     zh_en_tokenizer = transformers.AutoTokenizer.from_pretrained("AiratNazmiev/Helsinki-NLP-opus-mt-zh-en-tokenizer", local_files_only=False)
     zh_en_model = transformers.AutoModelForSeq2SeqLM.from_pretrained("AiratNazmiev/Helsinki-NLP-opus-mt-zh-en-model", local_files_only=False)
-    #zh_en_tokenizer.save_pretrained("./local_models/opus-mt-zh-en")
-    #zh_en_model.save_pretrained("./local_models/opus-mt-zh-en")
+    zh_en_tokenizer.save_pretrained("./local_models/opus-mt-zh-en")
+    zh_en_model.save_pretrained("./local_models/opus-mt-zh-en")
+
+    zh_en_tokenizer = transformers.AutoTokenizer.from_pretrained("./local_models/opus-mt-zh-en", local_files_only=True)
+    zh_en_model = transformers.AutoModelForSeq2SeqLM.from_pretrained("./local_models/opus-mt-zh-en", local_files_only=True)
     
     def translate(text: str, model, tokenizer) -> str:
         input_tokens = tokenizer(text, return_tensors="pt")
