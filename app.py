e93bab759abe97967e3d1a357bb4838b4aabaf46
Airat Nazmiev <43406093+AiratNazmiev@users.noreply.github.com>
2025-04-08 11:04:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -145,16 +145,13 @@ def text_preprocessing(text: str, ru_en_translator, zh_en_translator, language: 
 
 ###
 def main():
-    ru_en_translator = load_ru_en_translator()
-    zh_en_translator = load_zh_en_translator()
-    #ru_en_translator, zh_en_translator = None, None
-    msg2emoji_translator = load_msg2emoji_translator()
     if st.button("Translate"):
         if not text_value:
             st.warning("Please, enter the message 🤔", icon="⚠️")
         else:
+            msg2emoji_translator = load_msg2emoji_translator()
             emoji_text = msg2emoji_translator.translate(
-                text_preprocessing(text_value, ru_en_translator=ru_en_translator, zh_en_translator=zh_en_translator, language=language_abbr[language_option]),
+                text_preprocessing(text_value, ru_en_translator=load_ru_en_translator(), zh_en_translator=load_zh_en_translator(), language=language_abbr[language_option]),
                 sep='',
                 num_beams=5, 
                 do_sample=True, 
