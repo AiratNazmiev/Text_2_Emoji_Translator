7f88c8962537b4a68211d1305a1f6397fcaf84ea
Airat Nazmiev <43406093+AiratNazmiev@users.noreply.github.com>
2025-04-08 12:05:53 +0300

    old app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import torch
 import transformers
+from transformers import pipeline
 import re
 import streamlit as st
-import functools
-import os
-import multiprocessing
-
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 twitter_magic_number = 280
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -17,30 +13,69 @@ st.set_page_config(
     layout="centered",
 )
 
-def translate(text: str, model, tokenizer) -> str:
-    input_tokens = tokenizer(text, return_tensors="pt")
-    output_tokens = model.generate(**input_tokens)[0]
-    return tokenizer.decode(output_tokens, skip_special_tokens=True)
+st.markdown(
+    """
+    <style>
+    textarea:disabled {
+        opacity: 1 !important;                     
+        color: #000000 !important;
+        -webkit-text-fill-color: #FFFFFF !important;
+        background-color: inherit !important;
+        filter: none !important;
+    }
+    [data-testid="stTextArea"] label {
+        opacity: 1 !important;
+        color: #000000 !important;
+        -webkit-text-fill-color: #FFFFFF !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.title("Message 📝 to Emoji 😎 Translator")
+
+available_languages = {
+    "English 🦁" : ("en", "Enter the message to translate...", "English language"), 
+    "Russian 🐻" : ("ru", "Введите сообщение для перевода...", "Русский язык"), 
+    "Chinese 🐼" : ("zh", "输入要翻译的文本...", "中文")
+}
+language_abbr = {name : x[0] for name, x in available_languages.items()}
+language_placeholder = {name : x[1] for name, x in available_languages.items()}
+language_label = {name : x[2] for name, x in available_languages.items()}
+
+language_option = st.selectbox(
+    "Select language:",
+    available_languages.keys(),
+    index=0,
+    placeholder="Select language...",
+)
+
+text_value = st.text_area(
+    label=language_label[language_option],
+    placeholder=language_placeholder[language_option],
+    max_chars=twitter_magic_number,
+    help=f"Let's speak the language of facts 😉. Facts are limited to {twitter_magic_number} chars (twit size)",
+    height=150
+)
 
 ### load translators:
 @st.cache_resource
-def load_ru_en_translator():
-    ru_en_tokenizer = transformers.AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
-    ru_en_model = transformers.AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
+def load_translators():
+    ru_en_translator = pipeline(
+        "translation_ru_to_en", 
+        model="Helsinki-NLP/opus-mt-ru-en"
+    )
 
-    ru_en_translator = functools.partial(translate, model=ru_en_model, tokenizer=ru_en_tokenizer)
+    zh_en_translator = pipeline(
+        "translation_zh_to_en", 
+        model="Helsinki-NLP/opus-mt-zh-en"
+    )
     
-    return ru_en_translator
+    return ru_en_translator, zh_en_translator
 
-@st.cache_resource
-def load_zh_en_translator():
-    zh_en_tokenizer = transformers.AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-zh-en", local_files_only=False)
-    zh_en_model = transformers.AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-zh-en", local_files_only=False)
-
-    zh_en_translator = functools.partial(translate, model=zh_en_model, tokenizer=zh_en_tokenizer)
-    
-    return zh_en_translator
-
+ru_en_translator, zh_en_translator = load_translators()
+###
 
 ### create message-to-emoji translator wrapper class:
 @st.cache_resource
@@ -80,14 +115,16 @@ def load_msg2emoji_translator():
     )
     
     return msg2emoji_translator
+
+msg2emoji_translator = load_msg2emoji_translator()
 ###
 
 ### text preprocessing
-def text_preprocessing(text: str, ru_en_translator, zh_en_translator, language: str = 'en') -> str:
+def text_preprocessing(text: str, language: str = 'en') -> str:
     if language == 'ru':
-        text = ru_en_translator(text)
+        text = ru_en_translator(text)[0]['translation_text']
     elif language == 'zh':
-        text = zh_en_translator(text)
+        text = zh_en_translator(text)[0]['translation_text']
     
     if len(text) > twitter_magic_number:
         print(f"It's twit translator. The max length of the input is {twitter_magic_number} characters")
@@ -97,99 +134,41 @@ def text_preprocessing(text: str, ru_en_translator, zh_en_translator, language: 
     return text_re
 ###
 
-###
-def main():
-    st.markdown(
-        """
-        <style>
-        textarea:disabled {
-            opacity: 1 !important;                     
-            color: #000000 !important;
-            -webkit-text-fill-color: #FFFFFF !important;
-            background-color: inherit !important;
-            filter: none !important;
-        }
-        [data-testid="stTextArea"] label {
-            opacity: 1 !important;
-            color: #000000 !important;
-            -webkit-text-fill-color: #FFFFFF !important;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
+if st.button("Translate"):
+    if not text_value:
+        st.warning("Please, enter the message 🤔", icon="⚠️")
+    else:
+        emoji_text = msg2emoji_translator.translate(
+            text_preprocessing(text_value, language=language_abbr[language_option]),
+            sep='',
+            num_beams=5, 
+            do_sample=True, 
+            max_length=20
+        )
 
-    st.title("Message 📝 to Emoji 😎 Translator")
-
-    available_languages = {
-        "English 🦁" : ("en", "Enter the message to translate...", "English language"), 
-        "Russian 🐻" : ("ru", "Введите сообщение для перевода...", "Русский язык"), 
-        "Chinese 🐼" : ("zh", "输入要翻译的文本...", "中文")
-    }
-    language_abbr = {name : x[0] for name, x in available_languages.items()}
-    language_placeholder = {name : x[1] for name, x in available_languages.items()}
-    language_label = {name : x[2] for name, x in available_languages.items()}
-
-    language_option = st.selectbox(
-        "Select language:",
-        available_languages.keys(),
-        index=0,
-        placeholder="Select language...",
-    )
-
-    text_value = st.text_area(
-        label=language_label[language_option],
-        placeholder=language_placeholder[language_option],
-        max_chars=twitter_magic_number,
-        help=f"Let's speak the language of facts 😉. Facts are limited to {twitter_magic_number} chars (twit size)",
-        height=150
-    )
-    
-    msg2emoji_translator = load_msg2emoji_translator()
-    ru_en_translator = load_ru_en_translator()
-    zh_en_translator = load_zh_en_translator()
-    
-    if st.button("Translate"):
-        if not text_value:
-            st.warning("Please, enter the message 🤔", icon="⚠️")
-        else:
-            emoji_text = msg2emoji_translator.translate(
-                text_preprocessing(text_value, ru_en_translator=ru_en_translator, zh_en_translator=zh_en_translator, language=language_abbr[language_option]),
-                sep='',
-                num_beams=5, 
-                do_sample=True, 
-                max_length=20
-            )
-
-            st.text_area(
-                label="Emoji language",
-                placeholder="Translated texts will appear here.",
-                disabled=True,
-                value=emoji_text,
-            )
-            
-            st.markdown("""
-                <style>
-                .footer {
-                    position: fixed;
-                    bottom: 0;
-                    left: 0;
-                    width: 100%;
-                    background-color: #FFF1;
-                    text-align: center;
-                    padding: 15px;
-                    font-size: 15px;
-                    color: #FFFFFF;
-                }
-                </style>
-                <div class="footer">
-                    &copy; Nazmiev Airat 2025 👋
-
-                </div>
-            """, unsafe_allow_html=True
-            )
+        st.text_area(
+            label="Emoji language",
+            placeholder="Translated texts will appear here.",
+            disabled=True,
+            value=emoji_text,
+        )
         
-torch.classes.__path__ = []
-if __name__ == "__main__":
-    multiprocessing.freeze_support()
-    main()
+st.markdown("""
+    <style>
+    .footer {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        background-color: #FFF1;
+        text-align: center;
+        padding: 15px;
+        font-size: 15px;
+        color: #FFFFFF;
+    }
+    </style>
+    <div class="footer">
+        &copy; Nazmiev Airat 2025 👋
+
+    </div>
+""", unsafe_allow_html=True)
